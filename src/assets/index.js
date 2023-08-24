738111a1292ebf37056125ac70064840fca2dbeb
Ayush <unitedestates.ayush10@gmail.com>
2023-08-24 20:18:47 +0530

    assets import

---
+++ src/assets/index.js
@@ -0,0 +1,33 @@
+// self imports
+import close from "./close.svg";
+import facebook from "./facebook.png";
+import github from "./github.png";
+import github3d from "./github3D.png";
+import instagram from "./instagram.png";
+import leetcode from "./leetcode.png";
+import linkedin from "./linkedin.png";
+import logo from "./logo.svg";
+import menu from "./menu.svg";
+import mine from "./Mine.png";
+import project1 from "./project1.png";
+
+// tech imports
+import bootstrap from "./tech/Bootstrap.png";
+import cpp from "./tech/cpp.png";
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nextjs from "./tech/nextjs.png";
+import nodejs from "./tech/nodejs.png";
+import python from "./tech/python.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import sass from "./tech/sass.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.png";
+import redux from "./tech/redux.png";
+
+// company imports
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,34 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#050816",
+        secondary: "#aaa6c3",
+        tertiary: "#151030",
+        "black-100": "#100d25",
+        "black-200": "#090325",
+        "white-100": "#f3f3f3",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+      backgroundImage: {
+        // https://csstailwind.com/3-ways-to-add-background-image-in-tailwindcss/#:~:text=You%20can%20add%20a%20custom,that%20adds%20a%20background%20image.
+        //to add custom background image
+        "hero-pattern": "url('/src/assets/HeroBg.png')",
+        "about-pattern": "url('/src/assets/AboutBg.png')",
+        "work-pattern": "url('/src/assets/WorkBg.png')",
+        "skills-pattern": "url('/src/assets/SkillsBg.png')",
+        "project-pattern": "url('/src/assets/ProjectBg.png')",
+        "contact-pattern": "url('/src/assets/ContactBg.png')",
+      },
+    },
   },
   plugins: [],
-}
-
+};
