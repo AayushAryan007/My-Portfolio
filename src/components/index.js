659ca3a26695580756478caebc00b4ca1642ad6d
Ayush <unitedestates.ayush10@gmail.com>
2023-08-24 20:28:20 +0530

    component import complete +1

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,21 @@
 import { GreenBall, GreenCar } from "./Canvas";
+import Hero from "./Hero";
+import About from "./About";
+import Contact from "./Contact";
+import Experience from "./Experience";
+
+import Navbar from "./Navbar";
+import Projects from "./Projects";
+import Skills from "./Projects";
+
+export {
+  GreenBall,
+  GreenCar,
+  Hero,
+  About,
+  Contact,
+  Experience,
+  Navbar,
+  Projects,
+  Skills,
+};
