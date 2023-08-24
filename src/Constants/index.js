52196c169f60f8ae74be36d037b8d51163febf7a
Ayush <unitedestates.ayush10@gmail.com>
2023-08-24 20:36:57 +0530

    constant index prep

---
+++ src/Constants/index.js
@@ -0,0 +1,119 @@
+import {
+  close,
+  facebook,
+  github,
+  github3d,
+  instagram,
+  leetcode,
+  linkedin,
+  logo,
+  menu,
+  mine,
+  project1,
+  bootstrap,
+  cpp,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nextjs,
+  nodejs,
+  python,
+  reactjs,
+  redux,
+  sass,
+  tailwind,
+  threejs,
+  redux,
+  blackangles,
+  ncs,
+  ygp,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "project",
+    title: "Project",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "ReactJS",
+    icon: reactjs,
+  },
+  {
+    name: "NextJS",
+    icon: nextjs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "C++",
+    icon: cpp,
+  },
+  {
+    name: "Python",
+    icon: python,
+  },
+  {
+    name: "Sass CSS",
+    icon: sass,
+  },
+  {
+    name: "NodeJS",
+    icon: nodejs,
+  },
+  {
+    name: "Bootstrap",
+    icon: bootstrap,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "ThreeJS",
+    icon: threejs,
+  },
+  {
+    name: "Git",
+    icon: git,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Figma",
+    icon: figma,
+  },
+];
