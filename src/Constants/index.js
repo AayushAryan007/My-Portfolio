b32662b556e922ddde4c37e8a1cacb5feffab9d7
Ayush <unitedestates.ayush10@gmail.com>
2023-08-30 22:08:56 +0530

    extra

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -189,7 +189,7 @@ const roles = [
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Implementing responsive design and ensuring cross-browser accessibilty.",
     ],
   },
 ];
