a11825e93e055de830ace2fd3e27e8d741c0b062
Ayush <unitedestates.ayush10@gmail.com>
2023-09-25 21:16:15 +0530

    new start

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -46,7 +46,6 @@ const About = () => {
               <p className="  font-medium text-[18px]">
                 customers <br /> satisfied
               </p>
-              {/* extra */}
             </div>
             <div className="flex-1/4">
               <p className="numPlus-gradient  font-bold text-[50px]">+120</p>
