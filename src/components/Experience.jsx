026c40c14477e4887f158bc8556eacd4f1351507
Ayush <unitedestates.ayush10@gmail.com>
2023-09-26 22:24:48 +0530

    extra

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -18,9 +18,9 @@ const ExperienceCard = ({ experience }) => (
       background:
         "linear-gradient(226deg, rgba(167, 209, 41, 0.47) 0%, rgba(50, 166, 173, 0.37) 48.44%, rgba(129, 128, 210, 0.35) 100%)",
       color: "#fff",
-      border: "2px solid transparent", // Set a transparent border initially
+      border: "2px solid transparent",
       borderImage:
-        "linear-gradient(226deg, #a7d129 0%, #32a6ad 48.44%, #8180d2 100%) 1", 
+        "linear-gradient(226deg, #a7d129 0%, #32a6ad 48.44%, #8180d2 100%) 1",
     }}
     contentArrowStyle={{ borderRight: "12px solid #616F39" }}
     date={experience.date}
